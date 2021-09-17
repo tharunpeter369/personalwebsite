852c8960ca9f0df4d71e5ea590c0758c6c36aa90
tharun-peter <tharunpeter369@gmail.com>
2021-09-17 18:39:02 +0530

    git javascript not working

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -2,87 +2,87 @@
 
 
 console.log("its working")
-function validateName() {
+// function validateName() {
 
-    console.log('hello')
+//     console.log('hello')
 
-    var name = document.getElementById('name').value;
+//     var name = document.getElementById('name').value;
   
-    if(name.length == 0) {
+//     if(name.length == 0) {
   
-      producePrompt('Name is required', 'name-error' , 'red')
-      return false;
+//       producePrompt('Name is required', 'name-error' , 'red')
+//       return false;
   
-    }
+//     }
   
-    if (!name.match(/^[A-Za-z]+$/)) {
+//     if (!name.match(/^[A-Za-z]+$/)) {
   
-      producePrompt('Only Alphabets allowed','name-error', 'red');
-      return false;
+//       producePrompt('Only Alphabets allowed','name-error', 'red');
+//       return false;
   
-    }
+//     }
  
   
-    producePrompt('', 'name-error', 'green');
-    return true;
+//     producePrompt('', 'name-error', 'green');
+//     return true;
   
-  }
+//   }
 
 
-  function validateEmail () {
-      console.log('emal validation')
-    var email = document.getElementById('contact-email').value;
+//   function validateEmail () {
+//       console.log('emal validation')
+//     var email = document.getElementById('contact-email').value;
   
-    if(email.length == 0) {
+//     if(email.length == 0) {
   
-      producePrompt('Email Required','email-error', 'red');
-      return false;
+//       producePrompt('Email Required','email-error', 'red');
+//       return false;
   
-    }
+//     }
   
-    if(!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
-      producePrompt('Invalid Email', 'email-error', 'red');
-      return false;
+//     if(!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+//       producePrompt('Invalid Email', 'email-error', 'red');
+//       return false;
   
-    }
+//     }
   
-    producePrompt('', 'email-error', 'green');
-    return true;
+//     producePrompt('', 'email-error', 'green');
+//     return true;
   
-  }
+//   }
 
-  function validatePhone() {
+//   function validatePhone() {
 
-    var phone = document.getElementById('contact-phone').value;
-      if(phone.length == 0) {
-        producePrompt('Phone number is required.', 'phone-error', 'red');
-        return false;
-      }
+//     var phone = document.getElementById('contact-phone').value;
+//       if(phone.length == 0) {
+//         producePrompt('Phone number is required.', 'phone-error', 'red');
+//         return false;
+//       }
 
-      if(phone.length != 10) {
-        producePrompt('Enter a valid number', 'phone-error', 'red');
-        return false;
-      }
+//       if(phone.length != 10) {
+//         producePrompt('Enter a valid number', 'phone-error', 'red');
+//         return false;
+//       }
   
-      if(!phone.match(/^[0-9]{10}$/)) {
-        producePrompt('Only numbers Allowed' ,'phone-error', 'red');
-        return false;
-      }
+//       if(!phone.match(/^[0-9]{10}$/)) {
+//         producePrompt('Only numbers Allowed' ,'phone-error', 'red');
+//         return false;
+//       }
   
       
-      producePrompt('', 'phone-error', 'green');
-      return true;
+//       producePrompt('', 'phone-error', 'green');
+//       return true;
   
-  }
+//   }
 
 
 
 
 
-  function producePrompt(message, promptLocation, color) {
+//   function producePrompt(message, promptLocation, color) {
   
-    document.getElementById(promptLocation).innerHTML = message;
-    document.getElementById(promptLocation).style.color = color;
+//     document.getElementById(promptLocation).innerHTML = message;
+//     document.getElementById(promptLocation).style.color = color;
   
   
-  }
+//   }
